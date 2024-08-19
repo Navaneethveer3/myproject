0f390bacff27658aaf7e73901e64b8100c0ca459
Navaneethveer3 <navaneeth42.com@gmail.com>
2024-08-19 16:50:46 +0530

    settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -136,5 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://channel-1.onrender.onrender.com",
+    "https://channel-1.onrender.com",
 ]
